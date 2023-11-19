56f3b8afeb6ce4c82fc945d0a555f14c1515fc50
rafeiol <rafaelbamedi74@gmail.com>
2023-11-19 11:04:59 +0330

    ali

---
+++ n.py
@@ -0,0 +1,127 @@
+
+# # # a=input('num= ')
+# # # b=a.split(',')
+# # # for d in b:
+# # #     if int(d)%2!=0:
+# # #       print(d)
+
+# # # reshte=input('say=  ')
+# # # x=[]
+# # # x.extend(reshte)
+# # # x.reverse()
+# # # for a in x:
+# # #      print(a)
+# # a={
+# #     'username':'ali',
+# #     'password':'razi'
+    
+    
+    
+    
+# # }
+# # pas=a['password']
+# # user=a['username']
+# # ww=0
+# # while True:
+# #     print('///////////////////')
+# #     x= input('username=  ')
+# #     v=input('paswword=  ')
+# #     print(ww)
+    
+# #     if pas ==v and user == x:
+# #         print('welcome')
+# #         break
+# #     elif ww==3:
+# #        print('block')
+# #        break
+# #     else:
+# #         ww+=1
+# #         print('notok')
+# #         continue
+    
+# # 
+# # if number>=5:
+# #     print('bigthan5')
+# # else:
+# #     print('lesthan5')
+# # number=int(input("num=  "))
+# # if number>0:
+# #     print('+')
+# # elif number==0:
+# #     print('0')
+# # else:
+# #     print('-')
+# # 
+# # a=input("num=  ")
+# # b=a.split(',')
+# # print(b)
+# # for i in b:
+# #     if int(i)%2==0:
+# #         m=[i]
+# #         print(m)
+# # s=input("reshte= ")
+# # x=[]
+# # x.extend(s)
+# # x.reverse()
+# # for a in x:
+# #     print(a)
+
+# # a=input('nom= ')
+# # x=a.split(",")
+# # print(x)
+# # zz=0
+# # for z in x:
+# #     if int(z)>0:
+# #         zz+=1
+# #         print(z)
+# # رنامه ای بنویسید که یک لیست از اعداد را از کاربر بگیرد و تمام اعداد زوج را در یک لیست جدید ذخیره کرده و آن را چاپ کند
+# # a=add.split(',')
+# # for o in a:
+# #     if int(o)%2==0:
+# #         print(list(o))
+# # reshte=input('resh=  ')
+# # ww=[]
+# # ww.extend(reshte)
+# # ww.reverse()
+# # for q in ww:
+    
+# #     print(q)
+# # reshte=input('resh=  ')
+# # adad.split(',')
+# # w=[]
+# # w.extend(adad)
+# # for a in w:
+# #     if int(a)>0:
+# #         print(a)
+# #     else:
+# #         print('no')
+
+# # a=input('resh=  ')
+# # a.split(',')
+# # c=[a]
+# # for i in c:
+# #     if i%2==0:
+# #         print('oh')
+# #     else:
+# #         print('no') 
+# list_=[]
+# sum=0
+# count=-1
+# while True:
+#     num=  input("num=  ")
+#     count+=1
+#     list_.append(num)
+#     if num == 'end':
+#         break
+ 
+# print(list_)
+# list_.remove('end')
+# for a in list_:
+#     sum+=int(a)
+    
+# cc=sum/count
+    
+# print(sum)
+# print
+
+    
